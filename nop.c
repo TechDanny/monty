8304930a38ca2f8093ec1399ec6124691120d887
TechDanny <kokidanny15@gmail.com>
2023-06-22 14:25:31 -0700

    this function does not do anything

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - this function does not do anything
+ * @num_line:size of stack
+ * @arg:arguments
+ *
+ */
+
+void nop(unsigned int num_line, char *arg __attribute__((unused)));
+{
+
+}
